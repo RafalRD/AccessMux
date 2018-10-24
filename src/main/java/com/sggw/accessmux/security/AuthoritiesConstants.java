7e28bf49b0fd2ae8bd26452c494b7ea7dc473ebc
RafalRD <rafaldomalewski.rd@gmail.com>
2018-10-24 14:14:23 +0200

    add new roles

--- src/main/java/com/sggw/accessmux/security/AuthoritiesConstants.java
+++ src/main/java/com/sggw/accessmux/security/AuthoritiesConstants.java
@@ -11,6 +11,19 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String HR = "ROLE_HR";
+
+    public static final String IT = "ROLE_IT";
+
+    public static final String FINANSE = "ROLE_FINANSE";
+
+    public static final String MARKETING = "ROLE_MARKETING";
+
+    public static final String SPRZEDAZ = "ROLE_SPRZEDAZ";
+
+    public static final String INNE = "ROLE_INNE";
+
+
     private AuthoritiesConstants() {
     }
 }
